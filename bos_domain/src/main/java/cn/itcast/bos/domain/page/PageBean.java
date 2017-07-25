5b238093b0372bbed1b5b763e2c3eb220c60d2c7
hailong5214 <418338936@qq.com>
2017-07-25 15:58:43 +0800

    bos_domain的第一次提交

---
+++ bos_domain/src/main/java/cn/itcast/bos/domain/page/PageBean.java
@@ -0,0 +1,31 @@
+package cn.itcast.bos.domain.page;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+
+import cn.itcast.bos.domain.take_delivery.Promotion;
+
+@XmlRootElement(name="pageBean")
+@XmlSeeAlso({Promotion.class})
+public class PageBean<T> {
+	
+	private long totalCount;
+	
+	private List<T> pageData;
+	public long getTotalCount() {
+		return totalCount;
+	}
+	public void setTotalCount(long totalCount) {
+		this.totalCount = totalCount;
+	}
+	public List<T> getPageData() {
+		return pageData;
+	}
+	public void setPageData(List<T> pageData) {
+		this.pageData = pageData;
+	}
+	
+
+}
