bbf9310a4c0de6c1882743d1f459070fade6e5b4
hailong5214 <418338936@qq.com>
2017-07-29 11:38:14 +0800

    bos_domain new

---
+++ bos_domain/src/main/java/cn/itcast/bos/domain/constant/Contants.java
@@ -0,0 +1,8 @@
+package cn.itcast.bos.domain.constant;
+
+public class Contants {
+	public static final String BOS_FORE_URL="http://localhost:9003";
+	public static final String BOS_MANAGEMENT_URL="http://localhost:8080";
+	public static final String CRM_MANAGEMENT_URL="http://localhost:8888";
+	
+}
---
+++ bos_domain/src/test/java/bos_domain/df.java
@@ -0,0 +1,12 @@
+package bos_domain;
+
+import javax.swing.plaf.synth.SynthScrollBarUI;
+
+public class df {
+	public static void main(String[] args) {
+		String a ="123456789";
+		String b =a.replaceAll("123", "222");
+	  System.out.println(b);
+		
+	}
+}
