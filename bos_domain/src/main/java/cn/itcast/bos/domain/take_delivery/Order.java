34e9f901779617b07dc690f9f2723f77f783db5b
hailong5214 <418338936@qq.com>
2017-07-30 10:19:33 +0800

    11

--- bos_domain/src/main/java/cn/itcast/bos/domain/take_delivery/Order.java
+++ bos_domain/src/main/java/cn/itcast/bos/domain/take_delivery/Order.java
@@ -15,6 +15,8 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import org.apache.struts2.json.annotations.JSON;
+
 import cn.itcast.bos.domain.base.Area;
 import cn.itcast.bos.domain.base.Courier;
 
@@ -259,7 +261,7 @@ public class Order {
 	public void setWayBill(WayBill wayBill) {
 		this.wayBill = wayBill;
 	}
-
+	@JSON(serialize=false)
 	public Set<WorkBill> getWorkBills() {
 		return workBills;
 	}
